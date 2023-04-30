ce08bd483fdd75c538dc6ec25afb9712442d9e67
Jose Colomer <jococi99@gmail.com>
2023-04-30 17:41:08 +0200

    correccion de las fechas

--- main.ts
+++ main.ts
@@ -2,13 +2,11 @@ import { writeFile } from "fs/promises";
 import { loadFilms } from "./films.js";
 import { render, renderIndividual } from "./render_films.js";
 
-
 const films = await loadFilms(50);
-let i=1;
-for (const film of films){
-    writeFile(`film${i}.html`, renderIndividual(film));
-    i++;
+let i = 1;
+for (const film of films) {
+  writeFile(`film${i}.html`, renderIndividual(film));
+  i++;
 }
 const html = render(films);
 writeFile("index.html", html);
-
