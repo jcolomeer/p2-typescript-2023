b274c3b9eeed908879021024e171291bf389520f
Jose Colomer <jococi99@gmail.com>
2023-05-01 00:48:33 +0200

    directorio para html individuales

--- main.ts
+++ main.ts
@@ -1,11 +1,13 @@
 import { writeFile } from "fs/promises";
 import { loadFilms } from "./films.js";
 import { render, renderIndividual } from "./render_films.js";
+import { mkdirSync } from "fs";
 
 const films = await loadFilms(50);
 let i = 1;
+mkdirSync("./individual-files", { recursive: true });
 for (const film of films) {
-  writeFile(`film${i}.html`, renderIndividual(film));
+  writeFile(`./individual-files/film${i}.html`, renderIndividual(film));
   i++;
 }
 const html = render(films);
